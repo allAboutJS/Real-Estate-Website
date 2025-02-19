5d122c3ec2b5b396381eaa076f14696bbf2710e9
allAboutJS <victoronah1914@gmail.com>
2025-02-19 19:21:20 +0100

    add max-width and center alignment to Testimonials section

--- app/(public)/_components/Testimonials.jsx
+++ app/(public)/_components/Testimonials.jsx
@@ -18,7 +18,7 @@ export default function Testimonials() {
     ];
 
     return (
-        <section className="bg-stone-200 px-6 py-16">
+        <section className="bg-stone-200 px-6 py-16 max-w-screen-lg mx-auto">
             <h2 className="text-3xl text-center">What Our Customers Are Saying</h2>
             <p className="text-zinc-600 text-center">
                 We&apos;ve served numerous clients and here&apos;s what a few have got to say about us
