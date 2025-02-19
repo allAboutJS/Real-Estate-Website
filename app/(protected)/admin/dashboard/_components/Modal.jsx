b9ce65f91938cd01b239247a2a6cfe6304ef024b
allAboutJS <victoronah1914@gmail.com>
2025-02-19 21:26:10 +0100

    add modal for viewing messages and enhance MessageTable component

--- app/(protected)/admin/dashboard/_components/Modal.jsx
+++ app/(protected)/admin/dashboard/_components/Modal.jsx
@@ -1,9 +1,27 @@
-export default function Modal({ children }) {
+"use client";
+
+import { CgClose } from "react-icons/cg";
+
+export default function Modal({ children, onDismiss, heading }) {
     return (
         <>
-            <div className="inset-0 top-0 fixed bg-black bg-opacity-10 backdrop-blur-sm z-50"></div>
-            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-lg shadow-lg z-50">
-                <div>{children}</div>
+            <div onClick={onDismiss} className="inset-0 top-0 fixed bg-black bg-opacity-10 backdrop-blur-sm z-50"></div>
+            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white shadow-lg z-50 w-[90%] max-w-screen-sm h-fit max-h-[90%] overflow-y-auto">
+                <div className="relative">
+                    <button
+                        title="Close this modal"
+                        className="h-8 w-8 rounded-full bg-zinc-200 flex items-center justify-center absolute top-1 right-1 active:scale-90 transition-transform z-10"
+                        onClick={onDismiss}
+                    >
+                        <CgClose />
+                    </button>
+                    {heading && (
+                        <div className="text-xl bg-white font-semibold capitalize sticky top-0 w-full left-0 border-b p-4 text-center">
+                            {heading}
+                        </div>
+                    )}
+                    <div className="p-4">{children}</div>
+                </div>
             </div>
         </>
     );
--- app/(protected)/admin/dashboard/messages/_components/MessageTable.jsx
+++ app/(protected)/admin/dashboard/messages/_components/MessageTable.jsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import Modal from "../../_components/Modal";
+import { FaPhone, FaWhatsapp } from "react-icons/fa6";
+
 export default function MessageTable({ data }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentlyViewedMessage, setCurrentlyViewedMessage] = useState(null);
+
     return (
         <div className="overflow-auto max-w-full show-scrollbar">
             <table className="min-w-full">
@@ -32,7 +41,12 @@ export default function MessageTable({ data }) {
                             <td className="border-b py-4 px-1">{new Date(message.created_at).toDateString()}</td>
                             <td className="border-b py-4 px-1">
                                 <div className="flex gap-1 font-semibold">
-                                    <button className="px-2 py-1 bg-black text-white">View</button>
+                                    <button
+                                        onClick={() => (setCurrentlyViewedMessage(message), setIsModalOpen(true))}
+                                        className="px-2 py-1 bg-black text-white"
+                                    >
+                                        View
+                                    </button>
                                     <button className="px-2 py-1 bg-red-600 text-white">Delete</button>
                                 </div>
                             </td>
@@ -40,6 +54,59 @@ export default function MessageTable({ data }) {
                     ))}
                 </tbody>
             </table>
+            {isModalOpen && (
+                <Modal onDismiss={() => setIsModalOpen(false)} heading="View Message">
+                    {currentlyViewedMessage && (
+                        <div>
+                            <div className="space-y-3">
+                                <div className="bg-yellow-300 text-yellow-700 p-4 text-sm text-center font-semibold">
+                                    Please note that some numbers may not be registered on WhatsApp, and some people may
+                                    provide fake emails.
+                                </div>
+                                <div>
+                                    <div className="text-lg font-semibold">Fullname</div>
+                                    <div>{currentlyViewedMessage.fullname}</div>
+                                </div>
+                                <div>
+                                    <div className="text-lg font-semibold">Email</div>
+                                    <div>{currentlyViewedMessage.email}</div>
+                                </div>
+                                <div>
+                                    <div className="text-lg font-semibold">Phone</div>
+                                    <div>{currentlyViewedMessage.phone_number}</div>
+                                </div>
+                                <div>
+                                    <div className="text-lg font-semibold">Message</div>
+                                    <div>{currentlyViewedMessage.message}</div>
+                                </div>
+                                <div>
+                                    <div className="text-lg font-semibold">Sent At</div>
+                                    <div>{new Date(currentlyViewedMessage.created_at).toDateString()}</div>
+                                </div>
+                                <div>
+                                    <a
+                                        target="_blank"
+                                        href={`https://wa.me/${currentlyViewedMessage.phone_number.replace(
+                                            /^[0+]/,
+                                            ""
+                                        )}`}
+                                        className="bg-green-600 text-white p-2 flex items-center gap-2 justify-center"
+                                    >
+                                        <FaWhatsapp /> Chat on WhatsApp
+                                    </a>
+                                    <a
+                                        target="_blank"
+                                        href={`tel:${currentlyViewedMessage.phone_number}`}
+                                        className="bg-blue-600 text-white p-2 flex items-center gap-2 justify-center"
+                                    >
+                                        <FaPhone /> Dial the number
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </Modal>
+            )}
         </div>
     );
 }
