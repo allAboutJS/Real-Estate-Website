962b69ecb2a65860a5bbf142c2df78715a774601
allAboutJS <victoronah1914@gmail.com>
2025-02-20 14:03:06 +0100

    add unstable_noStore call in Messages component for caching optimization

--- app/(protected)/admin/dashboard/messages/page.jsx
+++ app/(protected)/admin/dashboard/messages/page.jsx
@@ -1,10 +1,11 @@
-import Modal from "../_components/Modal";
+import { unstable_noStore } from "next/cache";
 import Tabs from "../_components/Tabs";
 import getMessages from "./_actions/getMessages";
 import MessageTable from "./_components/MessageTable";
 import sortMessages from "./_utils/sortMessages";
 
 export default async function Messages() {
+    unstable_noStore();
     const { success, data } = await getMessages();
     const { all, read, unread } = sortMessages(success ? data : null);
 
@@ -28,7 +29,6 @@ export default async function Messages() {
                     ]}
                 />
             </div>
-            {/* <Modal>Hello World</Modal> */}
         </div>
     );
 }
