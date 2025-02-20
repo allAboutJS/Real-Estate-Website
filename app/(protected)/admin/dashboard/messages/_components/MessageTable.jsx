ba3656416d5682d11d7d49610d43da3e16fd1097
allAboutJS <victoronah1914@gmail.com>
2025-02-20 12:53:41 +0100

    implement message deletion with confirmation toast in MessageTable component

--- app/(protected)/admin/dashboard/messages/_components/MessageTable.jsx
+++ app/(protected)/admin/dashboard/messages/_components/MessageTable.jsx
@@ -5,7 +5,39 @@ import Modal from "../../_components/Modal";
 import { FaPhone, FaWhatsapp } from "react-icons/fa6";
 import { useRouter } from "next/navigation";
 import markMessageAsRead from "../_actions/markMessageAsRead";
-import handleMessageDeletion from "@/app/(protected)/admin/dashboard/messages/_utils/handleMessageDeletion";
+import { toast } from "sonner";
+import ToastOptions from "../../blog/[slug]/_components/ToastOptions";
+import deleteMessage from "../_actions/delateMessage";
+
+const handleMessageDeletion = async (messageId, router) => {
+    let toastId;
+    toastId = toast.warning("Are you sure you want to delete this message?", {
+        action: (
+            <ToastOptions
+                toastId={toastId}
+                onAccept={() =>
+                    toast.promise(
+                        () =>
+                            new Promise(async (resolve, reject) => {
+                                try {
+                                    toast.dismiss(toastId);
+                                    const { success } = await deleteMessage(messageId);
+                                    success ? (router.refresh(), resolve()) : reject();
+                                } catch {
+                                    reject();
+                                }
+                            }),
+                        {
+                            loading: "Deleting message...",
+                            success: "Message deleted successfully",
+                            error: "Failed to delete message"
+                        }
+                    )
+                }
+            />
+        )
+    });
+};
 
 export default function MessageTable({ data }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
