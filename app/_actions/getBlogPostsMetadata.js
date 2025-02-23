3119b099733f437a16d047f32733c738f4959d7a
allAboutJS <victoronah1914@gmail.com>
2025-02-23 18:48:43 +0100

    feat: enhance getBlogPostsMetadata function to include option for omitting hidden posts

--- app/_actions/getBlogPostsMetadata.js
+++ app/_actions/getBlogPostsMetadata.js
@@ -2,12 +2,14 @@
 
 import getDbClient from "@/app/_config/dbConfig";
 
-const getBlogPostsMetadata = async (limit) => {
+const getBlogPostsMetadata = async (limit, omitHidden = false) => {
     const client = getDbClient();
     try {
         await client.connect();
         const query =
-            "SELECT title, summary, slug, featured_image_url, created_at, last_updated_at, is_draft, archived FROM blog_posts";
+            omitHidden === true
+                ? "SELECT title, summary, slug, featured_image_url, created_at, last_updated_at, is_draft, archived FROM blog_posts WHERE is_draft = false AND archived = false"
+                : "SELECT title, summary, slug, featured_image_url, created_at, last_updated_at, is_draft, archived FROM blog_posts";
         const { rows } = limit ? await client.query(`${query} LIMIT $1`, [limit]) : await client.query(query);
 
         return { success: true, data: rows };
