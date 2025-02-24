d1dfc836ec8aa18071acf51a8b249753e6069bdf
allAboutJS <victoronah1914@gmail.com>
2025-02-24 06:12:37 +0100

    feat: add SSL configuration to database client for production environments

--- app/_config/dbConfig.js
+++ app/_config/dbConfig.js
@@ -4,7 +4,8 @@ export const getDbClient = () =>
     new Client({
         connectionString: process.env.DB_URL,
         connectionTimeoutMillis: 10_000,
-        query_timeout: 10_000
+        query_timeout: 10_000,
+        ssl: process.env.NODE_ENV === "production" ? true : undefined
     });
 
 export default getDbClient;
