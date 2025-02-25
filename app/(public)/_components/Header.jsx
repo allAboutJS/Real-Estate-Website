1b561cad0867d45dcc1bfe7b53c77a89edc8b1a8
allAboutJS <victoronah1914@gmail.com>
2025-02-25 18:54:11 +0100

    feat: update Header component with logo image and improved styling for better visibility

--- app/(public)/_components/Header.jsx
+++ app/(public)/_components/Header.jsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import Nav from "./Nav";
+import Image from "next/image";
+import { FaHouse } from "react-icons/fa6";
 
 export default function Header() {
     return (
         <header className="bg-gradient-to-b from-blue-200 via-blue-100 to-blue-50 shadow-sm sticky top-0 w-full left-0 right-0 z-20">
             <div className="flex justify-between max-w-screen-lg m-auto p-4 h-20 items-center">
                 <div>
-                    <Link href="/">Logo</Link>
+                    <Link className="flex text-blue-600 text-lg font-bold items-center" href="/">
+                        <img src="/images/logo.png" className="h-16 w-auto block" />
+                    </Link>
                 </div>
                 <Nav />
             </div>
