0efd29d21b2175fd8dfdfebcb863bca1e95e2227
allAboutJS <victoronah1914@gmail.com>
2025-02-27 10:57:52 +0100

    fix: update comparison operator in OtherProperties component to ensure correct filtering of properties

--- app/(public)/properties/[id]/_component/OtherProperties.jsx
+++ app/(public)/properties/[id]/_component/OtherProperties.jsx
@@ -11,7 +11,7 @@ export default async function OtherProperties({ id }) {
             <h3 className="text-2xl">Other Properties You May Be Interested In</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4 mt-6">
                 {success &&
-                    data.filter((post) => post.id !== id).map((post) => <PropertyCard key={post.id} {...post} />)}
+                    data.filter((post) => post.id != id).map((post) => <PropertyCard key={post.id} {...post} />)}
             </div>
         </div>
     );
